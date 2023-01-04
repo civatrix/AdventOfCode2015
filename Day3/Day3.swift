c9f33dbc90aed61b8147cf397fedb99c13523666
Daniel <daniel.johns@robotsandpencils.com>
2023-01-03 16:51:58 -0800

    Day 3 Part 1

--- Day3/Day3.swift
+++ Day3/Day3.swift
@@ -9,6 +9,21 @@ import Foundation
 
 final class Day3: Day {
     func run(input: String) -> String {
-        return ""
+        var set = Set<Point>([.zero])
+        
+        var point = Point.zero
+        for char in input {
+            switch char {
+            case "<": point += .left
+            case ">": point += .right
+            case "^": point += .up
+            case "v": point += .down
+            default: fatalError("Unknown direction: \(char)")
+            }
+            
+            set.insert(point)
+        }
+        
+        return set.count.description
     }
 }
--- Day3/Day3Tests.swift
+++ Day3/Day3Tests.swift
@@ -11,7 +11,17 @@ final class Day3Tests: XCTestCase {
     let day = Day3()
     
     func testDay() throws {
-        let input = ""
-        XCTAssertEqual(day.run(input: input), "")
+        let input = ">"
+        XCTAssertEqual(day.run(input: input), "2")
+    }
+    
+    func testDay2() throws {
+        let input = "^>v<"
+        XCTAssertEqual(day.run(input: input), "4")
+    }
+    
+    func testDay3() throws {
+        let input = "^v^v^v^v^v"
+        XCTAssertEqual(day.run(input: input), "2")
     }
 }
