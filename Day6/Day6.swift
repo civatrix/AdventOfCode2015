2b780795aff2bd15066e12529aad8b422db876e8
Daniel <daniel.johns@robotsandpencils.com>
2023-01-05 12:25:34 -0800

    Day 6 Part 1

--- Day6/Day6.swift
+++ Day6/Day6.swift
@@ -9,6 +9,34 @@ import Foundation
 
 final class Day6: Day {
     func run(input: String) -> String {
-        return ""
+        var lights = Set<Point>()
+        
+        for line in input.lines {
+            let digits = line.allDigits
+            let topLeft: Point = [digits[0], digits[1]]
+            let bottomRight: Point = [digits[2], digits[3]]
+            
+            (topLeft.x ... bottomRight.x).forEach { x in
+                (topLeft.y ... bottomRight.y).forEach { y in
+                    let point = Point(x: x, y: y)
+                    
+                    if line.starts(with: "turn on") {
+                        lights.insert(point)
+                    } else if line.starts(with: "turn off") {
+                        lights.remove(point)
+                    } else if line.starts(with: "toggle") {
+                        if lights.contains(point) {
+                            lights.remove(point)
+                        } else {
+                            lights.insert(point)
+                        }
+                    } else {
+                        fatalError("Invalid instruction: \(line)")
+                    }
+                }
+            }
+        }
+        
+        return lights.count.description
     }
 }
--- Day6/Day6Tests.swift
+++ Day6/Day6Tests.swift
@@ -11,7 +11,12 @@ final class Day6Tests: XCTestCase {
     let day = Day6()
     
     func testDay() throws {
-        let input = ""
-        XCTAssertEqual(day.run(input: input), "")
+        let input =
+"""
+turn on 0,0 through 999,999
+toggle 0,0 through 999,0
+turn off 499,499 through 500,500
+"""
+        XCTAssertEqual(day.run(input: input), "998996")
     }
 }
