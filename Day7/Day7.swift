07fba98089842a563f7d0f7e92e77c22bd2dc0a7
Daniel <daniel.johns@robotsandpencils.com>
2023-01-06 07:20:07 -0800

    Day 7 Part 1

--- Day7/Day7.swift
+++ Day7/Day7.swift
@@ -9,6 +9,42 @@ import Foundation
 
 final class Day7: Day {
     func run(input: String) -> String {
-        return ""
+        var lines = input.lines.map { $0.split(separator: " ") }
+        var map = [Substring: UInt16]()
+        
+        var lineIndex = 0
+        while map["a"] == nil {
+            let line = lines[wrapped: lineIndex]
+            
+            if line.count == 3, let lhs = map[line[0]] ?? UInt16(line[0]) {
+                map[line[2]] = lhs
+                lines.remove(at: lineIndex % lines.count)
+                lineIndex -= 1
+            } else if line.count == 4, let lhs = map[line[1]] {
+                map[line[3]] = ~lhs
+                lines.remove(at: lineIndex % lines.count)
+                lineIndex -= 1
+            } else if line[1] == "AND", let lhs = map[line[0]] ?? UInt16(line[0]), let rhs = map[line[2]] ?? UInt16(line[2]) {
+                map[line[4]] = lhs & rhs
+                lines.remove(at: lineIndex % lines.count)
+                lineIndex -= 1
+            } else if line[1] == "OR", let lhs = map[line[0]] ?? UInt16(line[0]), let rhs = map[line[2]] ?? UInt16(line[2]) {
+                map[line[4]] = lhs | rhs
+                lines.remove(at: lineIndex % lines.count)
+                lineIndex -= 1
+            } else if line[1] == "LSHIFT", let lhs = map[line[0]] {
+                map[line[4]] = lhs << UInt16(line[2])!
+                lines.remove(at: lineIndex % lines.count)
+                lineIndex -= 1
+            } else if line[1] == "RSHIFT", let lhs = map[line[0]] {
+                map[line[4]] = lhs >> UInt16(line[2])!
+                lines.remove(at: lineIndex % lines.count)
+                lineIndex -= 1
+            }
+            
+            lineIndex += 1
+        }
+        
+        return map["a"]!.description
     }
 }
--- Day7/Day7Tests.swift
+++ Day7/Day7Tests.swift
@@ -10,8 +10,58 @@ import XCTest
 final class Day7Tests: XCTestCase {
     let day = Day7()
     
-    func testDay() throws {
-        let input = ""
-        XCTAssertEqual(day.run(input: input), "")
+    func testAssign() throws {
+        let input =
+"""
+123 -> a
+"""
+        XCTAssertEqual(day.run(input: input), "123")
+    }
+    
+    func testAnd() throws {
+        let input =
+"""
+123 -> x
+456 -> y
+x AND y -> a
+"""
+        XCTAssertEqual(day.run(input: input), "72")
+    }
+    
+    func testOr() throws {
+        let input =
+"""
+123 -> x
+456 -> y
+x OR y -> a
+"""
+        XCTAssertEqual(day.run(input: input), "507")
+    }
+    
+    func testLShift() throws {
+        let input =
+"""
+123 -> x
+x LSHIFT 2 -> a
+"""
+        XCTAssertEqual(day.run(input: input), "492")
+    }
+    
+    func testRShift() throws {
+        let input =
+"""
+456 -> x
+x RSHIFT 2 -> a
+"""
+        XCTAssertEqual(day.run(input: input), "114")
+    }
+    
+    func testNot() throws {
+        let input =
+"""
+456 -> x
+NOT x -> a
+"""
+        XCTAssertEqual(day.run(input: input), "65079")
     }
 }
