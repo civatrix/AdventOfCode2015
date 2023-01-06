362e15b2538cde4d2cc4f0a43115a5a215590bde
Daniel <daniel.johns@robotsandpencils.com>
2023-01-06 08:14:01 -0800

    Day 10 Part 1

--- Day10/Day10.swift
+++ Day10/Day10.swift
@@ -9,6 +9,30 @@ import Foundation
 
 final class Day10: Day {
     func run(input: String) -> String {
-        return ""
+        var output = input.dropLast()
+        
+        for _ in 0 ..< 40 {
+            output = lookAndSay(output)
+        }
+        
+        return output.count.description
+    }
+    
+    func lookAndSay(_ input: Substring) -> Substring {
+        var output: Substring = ""
+        var current = input.first!
+        var length = 1
+        for char in input.dropFirst() {
+            if char == current {
+                length += 1
+            } else {
+                output += "\(length)\(current)"
+                length = 1
+                current = char
+            }
+        }
+        
+        output += "\(length)\(current)"
+        return output
     }
 }
