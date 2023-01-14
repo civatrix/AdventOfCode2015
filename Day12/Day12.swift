d0808f65129cdc43a91028c8e847791921c80064
Daniel <daniel.johns@robotsandpencils.com>
2023-01-13 19:51:06 -0800

    Day 12 Part 2

--- Day12/Day12.swift
+++ Day12/Day12.swift
@@ -9,6 +9,24 @@ import Foundation
 
 final class Day12: Day {
     func run(input: String) -> String {
-        return input.allDigits.sum.description
+        let json = try! JSONSerialization.jsonObject(with: input.data(using: .utf8)!)
+        return parse(json: json).description
+    }
+    
+    func parse(json: Any) -> Int {
+        if let array = json as? [Any] {
+            return array.map { parse(json: $0) }.sum
+        } else if let dict = json as? [String: Any] {
+            if dict.contains(where: { ($0.value as? String) == "red" }) {
+                return 0
+            }
+            return dict.map { parse(json: $0.value) }.sum
+        } else if let number = json as? Int {
+            return number
+        } else if let _ = json as? String {
+            return 0
+        } else {
+            fatalError("Unknown json type: \(json)")
+        }
     }
 }
--- Day12/Day12Tests.swift
+++ Day12/Day12Tests.swift
@@ -16,32 +16,17 @@ final class Day12Tests: XCTestCase {
     }
     
     func testDay2() throws {
-        let input = #"{"a":2,"b":4}"#
-        XCTAssertEqual(day.run(input: input), "6")
+        let input = #"[1,{"c":"red","b":2},3]"#
+        XCTAssertEqual(day.run(input: input), "4")
     }
     
     func testDay3() throws {
-        let input = "[[[3]]]"
-        XCTAssertEqual(day.run(input: input), "3")
+        let input = #"{"d":"red","e":[1,2,3,4],"f":5}"#
+        XCTAssertEqual(day.run(input: input), "0")
     }
     
     func testDay4() throws {
-        let input = #"{"a":{"b":4},"c":-1}"#
-        XCTAssertEqual(day.run(input: input), "3")
-    }
-    
-    func testDay5() throws {
-        let input = #"{"a":[-1,1]}"#
-        XCTAssertEqual(day.run(input: input), "0")
-    }
-    
-    func testDay6() throws {
-        let input = #"[-1,{"a":1}]"#
-        XCTAssertEqual(day.run(input: input), "0")
-    }
-    
-    func testDay7() throws {
-        let input = "[1,2,3]"
+        let input = #"[1,"red",5]"#
         XCTAssertEqual(day.run(input: input), "6")
     }
 }
