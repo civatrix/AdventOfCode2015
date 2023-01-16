09d1764c94309952fd754a0a90c1ef875b877cec
Daniel <daniel.johns@robotsandpencils.com>
2023-01-16 13:36:54 -0800

    Day 15 Part 1

--- Day15/Day15.swift
+++ Day15/Day15.swift
@@ -8,7 +8,56 @@
 import Foundation
 
 final class Day15: Day {
+    struct Ingredient {
+        static func +(_ lhs: Ingredient, _ rhs: Ingredient) -> Ingredient {
+            return .init(capacity: lhs.capacity + rhs.capacity, durability: lhs.durability + rhs.durability, flavour: lhs.flavour + rhs.flavour, texture: lhs.texture + rhs.texture)
+        }
+        
+        static func *(_ lhs: Ingredient, _ rhs: Int) -> Ingredient {
+            return .init(capacity: lhs.capacity * rhs, durability: lhs.durability * rhs, flavour: lhs.flavour * rhs, texture: lhs.texture *	 rhs)
+        }
+        
+        static let zero = Ingredient(capacity: 0, durability: 0, flavour: 0, texture: 0)
+        let capacity, durability, flavour, texture: Int
+        var score: Int {
+            guard capacity > 0, durability > 0, flavour > 0, texture > 0 else { return 0 }
+            return capacity * durability * flavour * texture
+        }
+    }
+    
     func run(input: String) -> String {
-        return ""
+        let ingredients = input.lines.map {
+            let numbers = $0.allDigits
+            return Ingredient(capacity: numbers[0], durability: numbers[1], flavour: numbers[2], texture: numbers[3])
+        }
+        
+        var maxScore = 0
+        for counts in mixtures(index: ingredients.count, total: 100) {
+            let score = zip(counts, ingredients)
+                .map { (count, ingredient) in
+                    return ingredient * count
+                }
+                .reduce(.zero, +)
+                .score
+            maxScore = max(score, maxScore)
+        }
+        
+        return maxScore.description
+    }
+    
+    func mixtures(index: Int, total: Int) -> [[Int]] {
+        let start = index == 1 ? total : 0
+        return (start ... total).flatMap { count in
+            let remaining = total - count
+            if index > 1 {
+                var mixes = [[Int]]()
+                for mixture in mixtures(index: index - 1, total: remaining) {
+                    mixes.append([count] + mixture)
+                }
+                return mixes
+            } else {
+                return [[count]]
+            }
+        }
     }
 }
--- Day15/Day15Tests.swift
+++ Day15/Day15Tests.swift
@@ -11,7 +11,18 @@ final class Day15Tests: XCTestCase {
     let day = Day15()
     
     func testDay() throws {
-        let input = ""
-        XCTAssertEqual(day.run(input: input), "")
+        let input =
+"""
+Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+"""
+        XCTAssertEqual(day.run(input: input), "62842880")
+    }
+    
+    func testMixtures() throws {
+        XCTAssertEqual(day.mixtures(index: 2, total: 10).count, 11)
+        let mixtures = day.mixtures(index: 3, total: 10)
+        XCTAssertTrue(mixtures.allSatisfy { $0.sum == 10 })
+        XCTAssertEqual(mixtures.count, 66)
     }
 }
