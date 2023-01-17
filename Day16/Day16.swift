9773a7ee041c0cb7677453b2b073afc9f86326dc
Daniel <daniel.johns@robotsandpencils.com>
2023-01-16 16:51:13 -0800

    Day 16 Part 1

--- Day16/Day16.swift
+++ Day16/Day16.swift
@@ -9,6 +9,71 @@ import Foundation
 
 final class Day16: Day {
     func run(input: String) -> String {
-        return ""
+        let target = (children: 3,
+                      cats: 7,
+                      samoyeds: 2,
+                      pomeranians: 3,
+                      akitas: 0,
+                      vizslas: 0,
+                      goldfish: 5,
+                      trees: 3,
+                      cars: 2,
+                      perfumes: 1)
+        
+        return input
+            .lines
+            .filter {
+                for match in $0.matches(of: /\ (\w+): (\d+)/) {
+                    let value = Int(match.output.2)!
+                    switch match.output.1 {
+                    case "children":
+                        if value != target.children {
+                            return false
+                        }
+                    case "cats":
+                        if value != target.cats {
+                            return false
+                        }
+                    case "samoyeds":
+                        if value != target.samoyeds {
+                            return false
+                        }
+                    case "pomeranians":
+                        if value != target.pomeranians {
+                            return false
+                        }
+                    case "akitas":
+                        if value != target.akitas {
+                            return false
+                        }
+                    case "vizslas":
+                        if value != target.vizslas {
+                            return false
+                        }
+                    case "goldfish":
+                        if value != target.goldfish {
+                            return false
+                        }
+                    case "trees":
+                        if value != target.trees {
+                            return false
+                        }
+                    case "cars":
+                        if value != target.cars {
+                            return false
+                        }
+                    case "perfumes":
+                        if value != target.perfumes {
+                            return false
+                        }
+                    default: fatalError("Unknown match: \(match) in \($0)")
+                    }
+                }
+                
+                return true
+            }
+            .first!
+            .allDigits[0]
+            .description
     }
 }
