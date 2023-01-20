bb55bb6687dc8583e46a8fadce7ec6dca29576d8
Daniel <daniel.johns@robotsandpencils.com>
2023-01-20 15:30:41 -0800

    Day 24 Part 1

--- Day24/Day24.swift
+++ Day24/Day24.swift
@@ -9,6 +9,37 @@ import Foundation
 
 final class Day24: Day {
     func run(input: String) -> String {
-        return ""
+        let digits = input.allDigits.sorted(by: >)
+        let target = digits.sum / 3
+        
+        let results = Set(groups(of: digits, totalling: target, allResults: true))
+            .filter { group in !groups(of: digits.filter { !group.contains($0) }, totalling: target, allResults: false).isEmpty }
+        
+        let minCount = results.map { $0.count }.min()!
+        
+        return results.filter { $0.count == minCount }
+            .map { $0.reduce(1, *) }
+            .min()!
+            .description
+    }
+    
+    func groups(of numbers: [Int], totalling total: Int, allResults: Bool) -> [[Int]] {
+        guard total > 0 else { return [] }
+        
+        var combinations = [[Int]]()
+        for number in numbers.filter({ $0 <= total }) {
+            if number == total {
+                combinations.append([total])
+            } else {
+                let results = groups(of: numbers.filter { $0 < number }, totalling: total - number, allResults: allResults)
+                if !results.isEmpty {
+                    combinations.append(contentsOf: results.map { $0 + [number] })
+                }
+            }
+            
+            if !combinations.isEmpty && !allResults { break }
+        }
+        
+        return combinations
     }
 }
--- Day24/Day24Tests.swift
+++ Day24/Day24Tests.swift
@@ -11,7 +11,7 @@ final class Day24Tests: XCTestCase {
     let day = Day24()
     
     func testDay() throws {
-        let input = ""
-        XCTAssertEqual(day.run(input: input), "")
+        let input = "1 2 3 4 5 7 8 9 10 11"
+        XCTAssertEqual(day.run(input: input), "99")
     }
 }
